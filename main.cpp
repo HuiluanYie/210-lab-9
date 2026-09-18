2b6ab85c303c961076ada7df9b2eece7917a9ac0
HuiluanYie <huiluanyie@gmail.com>
2026-09-18 15:22:13 -0700

    Commit 3

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ using namespace std;
 
 const int DAYS = 30;
 
+//Function prototype
+void print_temp(array<double, DAYS>);
+
 int main() {
     // declarations
     array<double, DAYS> temperatures; // an array of temperatures for 30 days
@@ -17,19 +20,26 @@ int main() {
     ifstream fin; 
     fin.open("data.txt");
     if (fin.good( )) {
-
         for (int i = 0; i < DAYS; i++)
         {
             fin >> temperatures[i];
         }
-        
         fin.close( ); // close the file  
 
         // use <array> member functions
+        // <array> objects can report their own size, unlike C-style arrays
+        cout << "1.  Size: " << temperatures.size() << endl;
 
+        
     }
     else
         cout << "File not found.\n";
     
     return 0;
+}
+
+//Function definition
+void print_temp(array<double, DAYS> temps)
+{
+    for (double t : temps) cout << t << " "; cout << endl;
 }

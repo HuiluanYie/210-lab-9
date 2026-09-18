1f2c090a4a2f21d75f09089d16d691529229d6d6
HuiluanYie <huiluanyie@gmail.com>
2026-09-18 16:48:03 -0700

    Finished

--- main.cpp
+++ main.cpp
@@ -39,22 +39,21 @@ int main() {
         print_arr(temperatures);
 
         // accessing individual elements
-        cout << "3.  2nd temperature: " << temperatures.at(2) << endl;
-        cout << "4.  3rd temperature: " << temperatures[3] << endl;
+        cout << "3.  2nd temperature: " << temperatures.at(1) << endl;
+        cout << "4.  3rd temperature: " << temperatures[2] << endl;
         cout << "5.  1st temperature: " << temperatures.front() << endl;
         cout << "6.  last temperature: " << temperatures.back() << endl;
         cout << "7.  Empty? " << (temperatures.empty() == 0 ? "False" : "True") << endl;
         cout << "8.  Address? " << temperatures.data() << endl;
 
         // find an element
-        // find an element
-        double target = 83; // search target
+        double target = 79; // search target
         array < double, temperatures.size() > ::iterator it; // declare iterator to point to the found element
         it = find(temperatures.begin(), temperatures.end(), target);
 
         cout << "9. looking for a day which temperature is " << target << endl;
         if (it != temperatures.end())
-            cout << "\tFound, day#" << it - temperatures.begin() << "'s temperature is " << target << endl;
+            cout << "\tFound, day#" << it - temperatures.begin() + 1 << "'s temperature is " << target << endl;
         else
             cout << " Not found.\n";
 
@@ -70,7 +69,8 @@ int main() {
         // find max & min & sum
         cout << "12. The maximun temperature is: " << * max_element(temperatures.begin(), temperatures.end()) << endl;
         cout << "13. The minimun temperature is: " << * min_element(temperatures.begin(), temperatures.end()) << endl;
-        cout << "14. The average temperature is: " << accumulate(temperatures.begin(), temperatures.end(), 0.0) / temperatures.size() << endl;
+        cout << "14. The average temperature is: " << accumulate(temperatures.begin(), temperatures.end(), 0.0) 
+                                                        / temperatures.size() << endl;
 
         // fill an array with one value
         array < double, DAYS > same;
@@ -102,8 +102,8 @@ int main() {
         }
         fin.close(); // close the file  
 
-        // use <array> member functions
-        // <array> objects can report their own size, unlike C-style arrays
+        // use <vector> member functions
+        // <vector> objects can report their own size
         cout << "1.  Days of temperature recorded: " << temperatures2.size() << endl;
 
         // range loop to access elements
@@ -119,14 +119,13 @@ int main() {
         cout << "8.  Address? " << temperatures2.data() << endl;
 
         // find an element
-        // find an element
         double target = 83; // search target
         vector < double > ::iterator it; // declare iterator to point to the found element
         it = find(temperatures2.begin(), temperatures2.end(), target);
 
         cout << "9. looking for a day which temperature is " << target << endl;
         if (it != temperatures2.end())
-            cout << "\tFound, day#" << it - temperatures2.begin() << "'s temperature is " << target << endl;
+            cout << "\tFound, day#" << it - temperatures2.begin() + 1 << "'s temperature is " << target << endl;
         else
             cout << " Not found.\n";
 
@@ -142,7 +141,8 @@ int main() {
         // find max & min & sum
         cout << "12. The maximun temperature is: " << * max_element(temperatures2.begin(), temperatures2.end()) << endl;
         cout << "13. The minimun temperature is: " << * min_element(temperatures2.begin(), temperatures2.end()) << endl;
-        cout << "14. The average temperature is: " << accumulate(temperatures2.begin(), temperatures2.end(), 0) / temperatures2.size() << endl;
+        cout << "14. The average temperature is: " << accumulate(temperatures2.begin(), temperatures2.end(), 0.0)
+                                                     / temperatures2.size() << endl;
 
         // fill an array with one value
         vector < double > same (DAYS, 70);
@@ -162,7 +162,7 @@ int main() {
 }
 
 //Function definition
-void print_vec(array < double, DAYS > arr) {
+void print_arr(array < double, DAYS > arr) {
     // print_temp() prints out the contents in the array
     // arguments: an array of doubles
     // returns: none

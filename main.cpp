e1052a0bd20ee13b7b3ae7f80873aa13de36f779
HuiluanYie <huiluanyie@gmail.com>
2026-09-18 15:01:46 -0700

    Commit 1

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>  // for sort(), find()
+#include <numeric>    // for accumulate()
+#include <array>
+using namespace std;
+
+
+int main() {
+    // declarations
+
+    // use <array> member functions
+
+    return 0;
+}
